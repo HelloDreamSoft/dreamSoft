c98ba0188c35bfdfe75e402337cf4ef1f1afbe98
user1 <user1@DESKTOP-LK3CGQG>
2020-06-24 09:22:04 +0900

    2020-06-24_09:21 encodingFilter and encryptFilter commit

--- dreamSoft/src/com/kh/dream/common/EncodingFilter.java
+++ dreamSoft/src/com/kh/dream/common/EncodingFilter.java
@@ -10,42 +10,27 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
 /**
- * Servlet Filter implementation class EncodingFilter
+ * @WebFilter 는 특정 url이나 서블릿을 거쳐갈 떄,
+ * 	해당하는 필터를 동작시켜라 라는 뜻이다.
  */
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// 인코딩
+		
 		request.setCharacterEncoding("UTF-8");
-		// response는 JSP 지시자 태그로 선언되기 때문에 사용 안해도 됨
-		// <%@ page . . .  %>
+		
 		
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
 	}
 
 }
